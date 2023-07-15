136c74d3307d8d7c9e310f4e4d7d5ba6f4da553c
YoungSeok Joung <people7874@gmail.com>
2023-07-15 23:13:44 +0900

    fileConvert

--- backend/db/data/fileConvert.js
+++ backend/db/data/fileConvert.js
@@ -2,8 +2,12 @@ const fs = require("fs");
 
 const fileConvert = {
     base64_encode: (file) => {
-        let bipmap = fs.readFileSync(file);
-        return new Buffer.alloc(bitmap).toString('base64');
+        // console.log(file)
+        let bitmap = fs.readFileSync(file);
+        // console.log('bitmap :', bitmap)
+        // console.log('bitmap.length :', bitmap.length)
+        return new Buffer.from(bitmap).toString('base64');
     }
 };
+
 module.exports = fileConvert;
