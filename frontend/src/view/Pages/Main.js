f0199ab740dd9e75df16d5c3983a8efc331d4d1d
YoungSeok Joung <people7874@gmail.com>
2023-08-10 01:15:30 +0900

    add Mainpage

--- frontend/src/view/Pages/Main.js
+++ frontend/src/view/Pages/Main.js
@@ -49,148 +49,47 @@ export default function Main () {
     return (
         <>
             <Row gutter={{xs: 8, sm: 16}} style={{backgroundColor: 'blue'}}>
-                <Title>Configuration</Title>
+                <Col>
+                  <Title>Configuration</Title>
+                </Col>
             </Row>
 
             <Row gutter={{xs: 8, sm: 16}} style={{backgroundColor: 'red'}}>
-                <Col gutter={{xs: 8, sm: 16}} style={{backgroundColor: 'yellow', width: "100%"}}>
-                    <Dropdown
-                        menu={{
-                        items,
-                        }}
-                    >
-                        <a onClick={(e) => e.preventDefault()}>
-                        <Space>
-                            Hover me
-                            <DownOutlined />
-                        </Space>
-                        </a>
-                    </Dropdown>
-                </Col>
-                
-                <Col gutter={{xs: 8, sm: 16}} style={{backgroundColor: 'green', width: "100%"}}>
-                    <Dropdown
-                        menu={{
-                        items,
-                        }}
-                    >
-                        <a onClick={(e) => e.preventDefault()}>
-                        <Space>
-                            Hover me
-                            <DownOutlined />
-                        </Space>
-                        </a>
-                    </Dropdown>
-                </Col>
-
-                <Row gutter={{xs: 8, sm: 16}} style={{backgroundColor: 'brown'}}>
-                    <Dropdown
-                        menu={{
-                        items,
-                        }}
-                    >
-                        <a onClick={(e) => e.preventDefault()}>
-                        <Space>
-                            Hover me
-                            <DownOutlined />
-                        </Space>
-                        </a>
-                    </Dropdown>
-                </Row>
-
-            </Row>
-
-            <Row gutter={{xs: 8, sm: 16}} style={{backgroundColor: 'purple'}}>
-                <Space wrap>
-                <Button type="OK">Primary Button</Button>
-                </Space>
-            </Row>
-
-            {/* <Row gutter={{xs: 8, sm: 16}} style={{backgroundColor: 'blue'}}>
-                <Col xs={12} sm={12} md={12} style={{backgroundColor: 'red', height: 200}}>
-                    <Dropdown
-                        menu={{
-                        items,
-                        }}
-                    >
-                        <a onClick={(e) => e.preventDefault()}>
-                        <Space>
-                            Hover me
-                            <DownOutlined />
-                        </Space>
-                        </a>
-                    </Dropdown>
-                </Col>
-
-                <Col xs={12} sm={12} md={12}>
-                    <Space wrap>
-                    <Button type="primary">Primary Button</Button>
-                    <Button>Default Button</Button>
-                    <Button type="dashed">Dashed Button</Button>
-                    <Button type="text">Text Button</Button>
-                    <Button type="link">Link Button</Button>
-                    </Space>
-                </Col>
-            </Row>
-
-            <Row gutter={{xs: 8, sm: 16}} style={{backgroundColor: 'blue'}}>
-                <Col xs={12} sm={12} md={12} style={{backgroundColor: 'red', height: 200}}>
-                    <Dropdown
-                        menu={{
-                        items,
-                        }}
-                    >
-                        <a onClick={(e) => e.preventDefault()}>
-                        <Space>
-                            Hover me
-                            <DownOutlined />
-                        </Space>
-                        </a>
-                    </Dropdown>
-                </Col>
-
-                <Col xs={12} sm={12} md={12}>
-                    <Space wrap>
-                    <Button type="primary">Primary Button</Button>
-                    <Button>Default Button</Button>
-                    <Button type="dashed">Dashed Button</Button>
-                    <Button type="text">Text Button</Button>
-                    <Button type="link">Link Button</Button>
-                    </Space>
-                </Col>
+              <Col style={{backgroundColor: 'green'}}>
+                <div>
+                  <Dropdown menu={{ items, }}>
+                      <a onClick={(e) => e.preventDefault()}>
+                        <Space> Hover me <DownOutlined /> </Space>
+                      </a>
+                  </Dropdown>
+                </div>
+                <div>
+                  <Dropdown menu={{ items, }}>
+                      <a onClick={(e) => e.preventDefault()}>
+                        <Space> Hover me <DownOutlined /> </Space>
+                      </a>
+                  </Dropdown>
+                </div>
+                <div style={{backgroundColor:'brown'}}>
+                  <Dropdown menu={{ items, }}>
+                      <a onClick={(e) => e.preventDefault()}>
+                        <Space> Hover me <DownOutlined /> </Space>
+                      </a>
+                  </Dropdown>
+                </div>
+              </Col>
             </Row>
 
             <Row gutter={{xs: 8, sm: 16}}>
-                <Space wrap>
-                    <Button type="primary">Primary Button</Button>
-                    <Button>Default Button</Button>
-                    <Button type="dashed">Dashed Button</Button>
-                    <Button type="text">Text Button</Button>
-                    <Button type="link">Link Button</Button>
-                    </Space>
-
+                <Col span={24}>
+                  <div style={{ width: '100%', textAlign: 'right'}}> 
+                    <Button type="OK">Primary Button</Button> 
+                  </div>
+                </Col>
             </Row>
-            
-            <Col xs={12} sm={12} md={12}>
-                <Row gutter={{xs: 8, sm: 16}}>
-                    <Radio.Group onChange={onChange} value={value}>
-                    <Radio value={1}>A</Radio>
-                    <Radio value={2}>B</Radio>
-                    <Radio value={3}>C</Radio>
-                    <Radio value={4}>D</Radio>
-                    </Radio.Group>
-                </Row>
-            </Col> */}
+
         </>    
     )
 }
-{/* <Row gutter={{xs: 8, sm: 16}}>
-    <Col xs={12} sm={12} md={12}>
-    <BreadCrumb />
-    </Col>
-    <Col xs={12} sm={12} md={12} style={{textAlign:"right"}}>
-    <Button />
-    </Col>
-</Row> */}
 
 
