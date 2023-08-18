4a5ed7e9806a4f68bf01dc467e32737a4f50910f
YoungSeok Joung <people7874@gmail.com>
2023-08-18 23:29:58 +0900

    add tabs

--- frontend/src/view/Component/Modal/ProblemModal.js
+++ frontend/src/view/Component/Modal/ProblemModal.js
@@ -1,12 +1,10 @@
 import React, {useState} from 'react'
-import { Modal, Button, Steps, theme, message, Row, Col, List } from 'antd'
+import { Modal, Button, Steps, theme, message, Row, Col, Tabs, Divider } from 'antd'
 import { useSelector } from 'react-redux'
 
 export default function ProblemModal({open, onClosed}) { 
 
-    const onModalClosed = ()=> { onClosed() }
-    
-    // (data fetched by useSelector >>> steps
+    // steps(data fetched by useSelector >>> steps
     const steps = [
         {
             title: 'Problem1',
@@ -21,10 +19,7 @@ export default function ProblemModal({open, onClosed}) {
             content:<Button >3</Button>,
         },
     ];
-
-    const data = [1,2,3];
-
-    const { token } = theme.useToken();
+    
     const [current, setCurrent] = useState(0);
     const next = () => {
         setCurrent(current + 1);
@@ -36,68 +31,65 @@ export default function ProblemModal({open, onClosed}) {
         key: item.title,
         title: item.title,
     }));
-    const contentStyle = {
-        lineHeight: '260px',
-        textAlign: 'center',
-        color: token.colorTextTertiary,
-        backgroundColor: token.colorFillAlter,
-        borderRadius: token.borderRadiusLG,
-        border: `1px dashed ${token.colorBorder}`,
-        marginTop: 16,
-    };
+
+    // tabs
+    const tabsItems = items.map((_, i) => {
+        const id = String(i+1);
+        // setCurrent(i)
+        return {
+            label: 'Problem' + id,
+            key: id
+        }
+    })
+    
+    // Modal
+    const onModalClosed = ()=> { onClosed() }
 
     const footer = 
-        <div style={{ marginTop: 24, }}>
-            {current > 0 && (
-                <Button
-                    style={{margin: '0 8px',}}
-                    onClick={() => prev()}
-                >
-                    Previous
-                </Button>
-            )}
-            {current < steps.length - 1 && (
-                <Button type="primary" onClick={() => next()}>
-                    Next
-                </Button>
-            )}
-            {current === steps.length - 1 && (
-                <Button type="primary" onClick={() => message.success('Processing complete!')}>
-                    Done
-                </Button>
-            )}
-        </div>
+    <div style={{ marginTop: 24, }}>
+        {current > 0 && (
+            <Button
+                style={{margin: '0 8px',}}
+                onClick={() => prev()}
+            >
+                Previous
+            </Button>
+        )}
+        {current < steps.length - 1 && (
+            <Button type="primary" onClick={() => next()}>
+                Next
+            </Button>
+        )}
+        {current === steps.length - 1 && (
+            <Button type="primary" onClick={() => message.success('Processing complete!')}>
+                Done
+            </Button>
+        )}
+    </div>
 
     return (
         <Modal title="Basic Modal" open={open} onCancel={onModalClosed}
                 footer={footer}>
-            <div>
-                <Row>
-                    <Col span={24} style={{ textAlign: 'center' }}>      
-                        <Steps size="small" current={current} items={items}/>        
-                    </Col>
-                </Row>
-
-                <Row>
-                    <Col span = {4} style={contentStyle}>
-                        <List
-                            bordered
-                            dataSource={data}
-                            renderItem={item => (
-                                <List.Item onClick={() => {setCurrent(item - 1)}}>
-                                    {item}
-                                </List.Item>
-                            )}
+            <Row span={24}>
+                <Col span={24}>
+                    <Row span={24}>
+                        <Col span={24}>
+                            <Steps size="small" current={current} items={items}/>     
+                        </Col>
+                    </Row>
+                    <Divider/>
+                    <Row span={24}>
+                        <Col>
+                            <Tabs size='small' defaultActiveKey="1" tabPosition={'left'} style={{ height: '100%'}}
+                                items={tabsItems}
                             />
-                    </Col>
-                    
-                    <Col span = {20}>
-                        {/* {steps[current].content} */}
-                        <div style={contentStyle}>{steps[current].content}</div>
-                    </Col>
-                </Row>
-
-            </div>
+                        </Col>
+                        <Col>      
+                            {steps[current].content}
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
         </Modal>
     )
 }
