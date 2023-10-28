e694b7731cf3ed856bcfa973f62a617bf2c84d53
suhoihn <Ihnsuho0819@gmail.com>
2023-10-28 16:06:25 +0700

    updates after the lesson

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,22 @@
+// Require all essential modules
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+
+// Additional modules
+var cors = require('cors');
+const db = require('./db/db.js'); // DB connection
+const dataRouter = require('./routes/data'); // Router
+
+
+// Create the app (backend)
 const app = express();
-var cors = require('cors')
-
-const db = require('./db/db.js'); // load db
-const dataRouter = require('./routes/data');
 
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -20,25 +25,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors()) // cool now everything is handled!
+app.use(cors());
 
-db(); // run db
 
+// Connect to DB
+db();
+
+
+// Use the router from routes/data.js
+// The backend is accessed through address [PORT]://api/Data/[Address]
 app.use('/api/Data', dataRouter);
 
 
-// catch 404 and forward to error handler
+// Default error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
